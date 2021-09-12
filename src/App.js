a8c5492904d819f73be48a6b8c0397412f048f1a
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-09-12 19:50:24 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h2>app component</h2>
+      <Home />
     </div>
   );
 }
